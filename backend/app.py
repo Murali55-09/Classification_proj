9d1f4f6c68fddb3c9f4b9cd2ef5a8864f50fda4e
Murali55-09 <muralidharkurer@gmail.com>
2026-01-05 22:08:49 +0530

    routing updation

--- backend/app.py
+++ backend/app.py
@@ -5,13 +5,24 @@ from PIL import Image
 import json
 import os
 
-app = Flask(__name__, template_folder="frontend/templates")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Load model
-model = tf.keras.models.load_model("cattle_breed_model.keras")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "../frontend/templates")
+STATIC_DIR = os.path.join(BASE_DIR, "../frontend/static")
+
+
+app = Flask(
+    __name__,
+    template_folder=TEMPLATE_DIR,
+    static_folder=STATIC_DIR
+)
+
+# 🔹 Load model safely
+model_path = os.path.join(BASE_DIR, "cattle_breed_model.keras")
+model = tf.keras.models.load_model(model_path)  
 
 # Load class labels
-with open("class_indices.json", "r") as f:
+with open(os.path.join(BASE_DIR, "class_indices.json"), "r") as f:
     class_indices = json.load(f)
 
 labels = {v: k for k, v in class_indices.items()}
